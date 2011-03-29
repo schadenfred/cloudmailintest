a6ec058578cbb4366db9d71439d0847ef3e9a698
Fred Schoeneman <fred.schoeneman@gmail.com>
2011-03-28 17:39:06 -0700

    added migrations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudmailinemailtest::Application.config.secret_token = '292c3b52c57eb336a4e4707841b3e327ddef707c4a7720eff60dfb24a11360b0731d72dbddbe918d4047f8d5034e0de7e3751f5060509e8456664ea13ae01e97'
